faef352c1c5dccfed618ab659aa58693a76e0236
João Patrocínio <jpatrocinio5436@hotmail.com>
2018-07-17 13:18:12 +0100

    basic chat

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ app.get('/', function(req, res) {
 
 io.on('connection', function (socket) {
     console.log('a user connected');
+
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg)
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('a user disconnected');
+    })
 });
 
 http.listen(3000, function() {
